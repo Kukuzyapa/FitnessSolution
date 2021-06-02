9ed760a3c5af625c41435bef60ccb9e72900da63
Вадим Попченко <popchenko4work@mail.ru>
2021-06-02 15:52:48 +0300

    Реализовано чтение существующего пользователя и добавление нового

--- MyFitness.BL/Model/User.cs
+++ MyFitness.BL/Model/User.cs
@@ -17,12 +17,12 @@ namespace MyFitness.BL.Model
         /// <summary>
         /// Пол.
         /// </summary>
-        public Gender Gender { get; }
+        public Gender Gender { get; set; }
 
         /// <summary>
         /// Дата рождения.
         /// </summary>
-        public DateTime BirthDate { get; }
+        public DateTime BirthDate { get; set; }
 
         /// <summary>
         /// Вес.
@@ -33,6 +33,8 @@ namespace MyFitness.BL.Model
         /// Рост.
         /// </summary>
         public double Height { get; set; }
+
+        public int Age { get { return DateTime.Now.Year - BirthDate.Year; } }
         #endregion
 
         /// <summary>
@@ -83,9 +85,19 @@ namespace MyFitness.BL.Model
             Height = height;
         }
 
+        public User(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null.", nameof(name));
+            }
+            Name = name;
+
+        }
+
         public override string ToString()
         {
-            return Name;
+            return Name + " " + Age;
         }
     }
 }
