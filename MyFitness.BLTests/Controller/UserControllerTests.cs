889e62ac2e4e0c5e9b7e6f3f14b1f52a72f60b8d
Вадим Попченко <popchenko4work@mail.ru>
2021-06-03 20:54:00 +0300

    Добавлено модульное тестирование для контроллера пользователя

---
+++ MyFitness.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFitness.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            // Arrage
+            var userName = Guid.NewGuid().ToString();
+            var height = 180;
+            var weight = 80;
+            var birthDate = DateTime.Now.AddYears(-18);
+            var gender = "man";
+            var controller = new UserController(userName);
+
+            // Act
+            controller.SetNewUserData(gender, birthDate, weight, height);
+            var controller2 = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+            Assert.AreEqual(height, controller2.CurrentUser.Height);
+            Assert.AreEqual(weight, controller2.CurrentUser.Weight);
+            Assert.AreEqual(birthDate, controller2.CurrentUser.BirthDate);
+            Assert.AreEqual(gender, controller2.CurrentUser.Gender.Name);
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            // Arrage
+            var userName = Guid.NewGuid().ToString();
+
+            // Act
+            var controller = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
