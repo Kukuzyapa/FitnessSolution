5f1faac53300305392e26410c838709340ee6e6e
Вадим Попченко <popchenko4work@mail.ru>
2021-06-04 23:30:21 +0300

    Реализовано добавление приемов пищи и продуктов питания

--- MyFitness.CMD/Program.cs
+++ MyFitness.CMD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MyFitness.BL.Controller;
+using MyFitness.BL.Model;
 
 namespace MyFitness.CMD
 {
@@ -13,6 +14,7 @@ namespace MyFitness.CMD
             var name = Console.ReadLine();
 
             var userController = new UserController(name);
+            var eatingController = new EatingController(userController.CurrentUser);
             if (userController.IsNewUser)
             {
                 Console.Write("Введите пол: ");
@@ -25,7 +27,40 @@ namespace MyFitness.CMD
             }
 
             Console.WriteLine(userController.CurrentUser);
+
+            Console.WriteLine("Что вы хотите сделать?");
+            Console.WriteLine("E - Ввести прием пищи");
+            var key = Console.ReadKey();
+            Console.WriteLine();
+
+            if(key.Key == ConsoleKey.E)
+            {
+                var foods = EnterEating();
+                eatingController.Add(foods.Food, foods.Weight);
+
+                foreach(var item in eatingController.Eating.Foods)
+                {
+                    Console.WriteLine($"\t{item.Key} - {item.Value}");
+                }
+            }
+
             Console.ReadKey();
+        }
+
+        private static (Food Food, double Weight) EnterEating()
+        {
+            Console.Write("Введите имя продукта: ");
+            var food = Console.ReadLine();
+
+            var calories = ParseDouble("калорийность");
+            var prots = ParseDouble("белки");
+            var fats = ParseDouble("жиры");
+            var carbs = ParseDouble("углеводы");
+
+            var weight = ParseDouble("вес порции");
+            var product = new Food(food, calories, prots, fats, carbs);
+
+            return (Food: product, Weight: weight);
         }
 
         private static DateTime ParseDateTime()
@@ -58,7 +93,7 @@ namespace MyFitness.CMD
                 }
                 else
                 {
-                    Console.WriteLine($"Неверный формат {name}");
+                    Console.WriteLine($"Неверный формат поля {name}");
                 }
             }
         }
